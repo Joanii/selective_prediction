49db7353f113e82fb2c3747ad899e77fe7ed668e
Joanii <kuehne.joana@web.de>
2023-07-21 10:29:08 +0200

    Added some docstrings

--- src/selective_prediction.py
+++ src/selective_prediction.py
@@ -1,11 +1,14 @@
-import os
-import torch
-
-from src.utils import *
 from src.plotting import plot_risk_over_coverage
+from src.utils import *
 
 
 def run_softmax(predict, target):
+    """
+    Run softmax prediction in which samples with low softmax response values are rejected.
+    :param predict: prediction of classifier
+    :param target: target classes
+    :return: risk and coverage for different thresholds
+    """
     softmax_response = get_softmax_response(predict)
     softmax_response = softmax_response.detach().numpy().squeeze()
     risk, coverage = iterate_thresholds(1 - softmax_response, predict, target)
@@ -13,6 +16,15 @@ def run_softmax(predict, target):
 
 
 def run_joint_model(predict, second_predict, target):
+    """
+    Run Joint Model prediction in which two propagations are conducted through the classifier for each sample, one with
+    the org. input data and one with the VAE reconstruction. If the two predicted classes are not identical, the sample
+    is rejected.
+    :param predict: prediction of classifier
+    :param second_predict: prediction of classifier based on the reconstruction
+    :param target: target classes
+    :return: risk and coverage (single values)
+    """
     _, predicted = predict.max(1)
     _, second_predicted = second_predict.max(1)
     joint_mask = predicted.squeeze() == second_predicted.squeeze()
@@ -23,18 +35,36 @@ def run_joint_model(predict, second_predict, target):
     return risk, coverage
 
 
-def run_joint_softmax(output, second_output, target):
-    softmax = get_softmax(output)
+def run_joint_softmax(prediction, second_prediction, target):
+    """
+    Run Joint Softmax prediction which is a combination of the Joint Model and Softmax approaches. Samples are rejected
+    if the two predictions are not identical and if the softmax responses of the two predictions are far apart.
+    :param prediction: prediction of classifier
+    :param second_prediction: prediction of classifier based on reconstruction
+    :param target: target classes
+    :return: risk and coverage for different thresholds
+    """
+    softmax = get_softmax(prediction)
     softmax_response, predicted = softmax.max(1)
-    second_softmax = get_softmax(second_output)
+    second_softmax = get_softmax(second_prediction)
     second_softmax_response, second_predicted = second_softmax.max(1)
     diff = torch.abs(second_softmax_response - softmax_response)
     diff[predicted != second_predicted] = 1
-    risk, coverage = iterate_thresholds(diff.detach().numpy(), output, target)
+    risk, coverage = iterate_thresholds(diff.detach().numpy(), prediction, target)
     return risk, coverage
 
 
 def run_selective_net(selective_model, sel_data, target, attack_name, epsilon):
+    """
+    Run Selective Net based on https://arxiv.org/abs/1901.09192.
+    :param selective_model: neural network model
+    :param sel_data: input data (if gradient based adversarial attack is applied this differs from input data of
+    classifier.
+    :param target: target classes
+    :param attack_name: name of the attack
+    :param epsilon: magnitude of the attack
+    :return: risk and coverage for different thresholds
+    """
     sel_atk = initialize_attack(attack_name, selective_model, epsilon)
     adv_samples_sel = sel_atk(sel_data, target)
     p_pred, s_pred, _ = selective_model(adv_samples_sel, all_outputs=True)
@@ -42,13 +72,25 @@ def run_selective_net(selective_model, sel_data, target, attack_name, epsilon):
     return sel_risk, sel_coverage
 
 
-def run_latent_selection(output, targets, mu, recon, samples, vae, ref_dataloader):
+def run_latent_selection(predict, targets, mu, recon, samples, vae, ref_dataloader):
+    """
+    Run latent selection which discards samples which are far from the reference samples (for the same class as
+    predicted) in the latent space of the VAE and considers the reconstruction loss of the VAE as well.
+    :param predict: prediction of classifier
+    :param targets: target classes
+    :param mu: mu values of VAE prediction
+    :param recon: reconstruction of VAE
+    :param samples: input samples
+    :param vae: VAE model
+    :param ref_dataloader: dataloader with reference data (e.g. validation dataloader)
+    :return:
+    """
     alpha = 0.2
     ref_data, ref_targets = get_data_from_loader(ref_dataloader)
     ref_recon, ref_mu, ref_sigma = vae(ref_data)
-    distances = torch.empty(output.shape[0])
-    recon_loss = torch.empty(output.shape[0])
-    _, predicted_classes = output.squeeze().max(1)
+    distances = torch.empty(predict.shape[0])
+    recon_loss = torch.empty(predict.shape[0])
+    _, predicted_classes = predict.squeeze().max(1)
     mu_concat = torch.cat((mu, ref_mu))
     mu_concat_tsne = torch.Tensor(tsne_reduce_dimensions(mu_concat))
     mu_tsne, ref_mu_tsne = mu_concat_tsne[:mu.shape[0], :], mu_concat_tsne[mu.shape[0]:, :]
@@ -58,12 +100,25 @@ def run_latent_selection(output, targets, mu, recon, samples, vae, ref_dataloade
         distances[idx] = torch.linalg.vector_norm(mu_i - ref_mu_tsne_class, dim=1).min()
         recon_loss[idx] = (torch.linalg.vector_norm(recon[idx] - sample)) ** 2 / torch.linalg.vector_norm(sample)
     loss = (1-alpha) * distances + alpha * recon_loss
-    risks, coverages = iterate_thresholds(loss.detach().numpy(), output, targets)
+    risks, coverages = iterate_thresholds(loss.detach().numpy(), predict, targets)
     return risks, coverages
 
 
 def run_selective_prediction(clf, vae, sel, selection_algorithms, datamodule, attack_name, epsilon,
                              folder_path='results'):
+    """
+    Run selective prediction with several selection approaches. The output is a plot displaying risk over coverage for
+    the respective approaches with varying thresholds applied.
+    :param clf: classifier model
+    :param vae: VAE model
+    :param sel: selective net model
+    :param selection_algorithms: dictionary with selection algorithms, indicating which ones are evaluated
+    :param datamodule: pytorch lightning datamodule
+    :param attack_name: name of adversarial attack, e.g. Clean (no attack), FGSM
+    :param epsilon: magnitude of attack
+    :param folder_path: path to folder for saving the plot
+    :return:
+    """
     atk = initialize_attack(attack_name, clf, epsilon)
     coverages = {}
     risks = {}
@@ -103,12 +158,14 @@ if __name__ == '__main__':
     from src.utils import get_latest_checkpoint
     import argparse
 
-    parser = argparse.ArgumentParser(description='Train neural network models')
+    parser = argparse.ArgumentParser(description='Run selective prediction with a number of different selection '
+                                                 'approaches')
     parser.add_argument('--attack', required=True, help='Adversarial attack, e.g. pgd, fgsm, vanilla')
     parser.add_argument('--epsilon', required=True, type=float, help='magnitude of attack')
     args = parser.parse_args()
     vae = VAE.load_from_checkpoint(get_latest_checkpoint(os.path.join('models', 'vae', 'lightning_logs'))).eval()
     clf = Classifier.load_from_checkpoint(get_latest_checkpoint(os.path.join('models', 'clf', 'lightning_logs'))).eval()
-    sel = SelectiveNet.load_from_checkpoint(get_latest_checkpoint(os.path.join('models', 'sel', 'lightning_logs'))).eval()
+    sel = SelectiveNet.load_from_checkpoint(get_latest_checkpoint(os.path.join('models', 'sel',
+                                                                               'lightning_logs'))).eval()
     selection_algorithms = {'Softmax': True, 'VAE': True, 'Sel': True, 'Joint': True, 'Joint-S': True, 'Latent': True}
     run_selective_prediction(clf, vae, sel, selection_algorithms, MnistDataModule(), args.attack, args.epsilon)
